1199202226f1053f7d7553f84fb9b26fd555dd37
Wagner Gomes Gonçalves <43182168+wagnergoncalves@users.noreply.github.com>
2021-07-03 16:44:21 -0300

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,45 +27,7 @@
 
 <body data-spy="scroll" data-target="#ftco-navbar" data-offset="200">
 
-    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-        <div class="container">
-            <a class="navbar-brand" href="javasript:;"><img style="width: 180px"
-                    src="{{ asset('images/logo-invert.png') }}" /></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
-                aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="oi oi-menu"></span> Menu
-            </button>
-
-            <div class="collapse navbar-collapse" id="ftco-nav">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active"><a href="#section-home" class="nav-link">Home</a></li>
-                    <li class="nav-item"><a href="#section-features" class="nav-link">Features</a></li>
-                    <li class="nav-item"><a href="#section-services" class="nav-link">Services</a></li>
-                    <li class="nav-item"><a href="#section-pricing" class="nav-link">Pricing</a></li>
-                    <li class="nav-item"><a href="#section-about" class="nav-link">About</a></li>
-                    <li class="nav-item"><a href="#section-contact" class="nav-link">Contact</a></li>
-
-
-                    @if (Route::has('login'))
-                        @auth
-                            <li class="nav-item"><a href="{{ url('/dashboard') }}" class="nav-link">Acesse seu
-                                    delivery</a></li>
-                        @else
-                            <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">Entre</a></li>
-
-                            @if (Route::has('register'))
-                                <li class="nav-item"><a href="{{ route('register') }}"
-                                        class="ml-4 nav-link">Cadastre-se</a></li>
-                            @endif
-                        @endif
-                        @endif
-
-
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <!-- END nav -->
+    @include('header')
 
         <section class="ftco-cover ftco-slant" style="background-image: url({{ asset('landing/images/bg_3.jpg') }});"
             id="section-home">
@@ -541,65 +503,8 @@
                 </div>
             </div>
         </section>
-        <footer class="ftco-footer ftco-bg-dark">
-            <div class="container">
-                <div class="row mb-5">
-                    <div class="col-md-8">
-                        <div class="row">
-                            <div class="col-md">
-                                <div class="ftco-footer-widget mb-4">
-                                    <h2 class="ftco-heading-2">Company</h2>
-                                    <ul class="list-unstyled">
-                                        <li><a href="#" class="py-2 d-block">About</a></li>
-                                        <li><a href="#" class="py-2 d-block">Jobs</a></li>
-                                        <li><a href="#" class="py-2 d-block">Press</a></li>
-                                        <li><a href="#" class="py-2 d-block">News</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="col-md">
-                                <div class="ftco-footer-widget mb-4">
-                                    <h2 class="ftco-heading-2">Communities</h2>
-                                    <ul class="list-unstyled">
-                                        <li><a href="#" class="py-2 d-block">Support</a></li>
-                                        <li><a href="#" class="py-2 d-block">Sharing is Caring</a></li>
-                                        <li><a href="#" class="py-2 d-block">Better Web</a></li>
-                                        <li><a href="#" class="py-2 d-block">Good Template</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="col-md">
-                                <div class="ftco-footer-widget mb-4">
-                                    <h2 class="ftco-heading-2">Useful links</h2>
-                                    <ul class="list-unstyled">
-                                        <li><a href="#" class="py-2 d-block">Bootstrap 4</a></li>
-                                        <li><a href="#" class="py-2 d-block">jQuery</a></li>
-                                        <li><a href="#" class="py-2 d-block">HTML5</a></li>
-                                        <li><a href="#" class="py-2 d-block">Sass</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="ftco-footer-widget mb-4">
-                            <ul class="ftco-footer-social list-unstyled float-md-right float-lft">
-                                <li><a href="#"><span class="icon-twitter"></span></a></li>
-                                <li><a href="#"><span class="icon-facebook"></span></a></li>
-                                <li><a href="#"><span class="icon-instagram"></span></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md text-left">
-                        <p>&copy; Exclusivity 2017. All Rights Reserved. Made with <span
-                                class="icon-heart text-danger"></span> by <a
-                                href="https://free-template.co/">Free-Template.co</a></p>
-                    </div>
-                </div>
-            </div>
-        </footer>
+
+        @include('footer')
 
         <!-- loader -->
         <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
